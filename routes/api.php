6d13d7167a6a4d23cd734944f45751d5a0d7b223
Jeni <jeni@gmail.com>
2026-09-20 09:33:20 +0700

    Implement Blog API BREAD

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,80 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Blog;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// // fungsi untuk mendapatkan user, but need authentikasi
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
 
-// disini http method get, menampikan hello world
-// dengan parameter name, by default adalah Ahmad
-Route::get('/hello-world', function() {
-   $name = request()->get('name') ?? 'Jeni'; 
+// BREAD
+// B = BROWSE (menampilkan list data)
+// R = READ (menampilkan detail data)
+// E = EDIT (mengubah data)
+// A = ADD (menambahkan data)
+// D = DELETE (menghapus data)
+
+
+// blogs : untuk menampikan semua data blogs (GET)
+// B (Browse): MENAMPILKAN LISTS DARIPADA BLOG YANG ADA DI DATABASE
+Route::get('/blogs', function() {
+    $blogs = Blog::get();
 
     return response()->json([
-        'message' => "Hello World, {$name}"
+        'data' => $blogs
     ]);
 });
 
-// blogs : untuk menampikan semua data blogs (GET)
-// blogs : untuk menambahkan data blog baru (POST)
 // blog/{id} : untuk menampilkan data blog berdasarkan id (GET)
+// R (Read): MENAMPILKAN DETAIL DATA BLOG BERDASARKAN ID
+Route::get('/blog/{id}', function($id) {
+    $blog = Blog::findOrFail($id);
+
+    return response()->json([
+        'message' => "Detail blog berhasil ditampilkan",
+        'data' => $blog
+    ]);
+});
+
 // blog/{id} : untuk mengupdate data blog berdasarkan id (PUT)
+// E (Edit): MENGUBAH DATA BLOG BERDASARKAN ID
+Route::put('/blog/{id}', function($id) {
+    $blog = Blog::findOrFail($id);
+
+    $blog->update([
+        'title' => request()->get('title'),
+        'content' => request()->get('content'),
+    ]);
+
+    return response()->json([
+        'message' => "Blog berhasil diubah",
+    ]);
+});
+
+
+// blogs : untuk menambahkan data blog baru (POST)
+// A (Add): MENAMBAHKAN DATA BLOG BARU KE DALAM DATABASE
+Route::post('/blogs', function() {
+    $blog = Blog::create([
+        'title' => request()->get('title'),
+        'content' => request()->get('content'),
+    ]);
+
+    return response()->json([
+        'message' => "Blog berhasil ditambahkan",
+        'data' => $blog
+    ]);
+});
+
 // blog/{id} : untuk menghapus data blog berdasarkan id (DELETE)
+// D (Delete): MENGHAPUS DATA BLOG BERDASARKAN ID
+Route::delete('/blog/{id}', function($id) {
+    $blog = Blog::findOrFail($id);
+
+    $blog->delete();
+
+    return response()->json([
+        'message' => "Blog berhasil dihapus",
+    ]);
+});
